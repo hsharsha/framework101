bed3916bc27f8511b0680e4a5de16c9651c9b122
Harsha HS <harsha.havanurshamsundara@amd.com>
2024-02-13 14:15:55 +0000

    HIP Hello world

---
+++ utils.hpp
@@ -0,0 +1,24 @@
+#ifndef COMMON_EXAMPLE_UTILS_HPP
+#define COMMON_EXAMPLE_UTILS_HPP
+
+#include <iostream>
+
+#include <hip/hip_runtime.h>
+
+constexpr int error_exit_code = -1;
+
+/// \brief Checks if the provided error code is \p hipSuccess and if not,
+/// prints an error message to the standard error output and terminates the program
+/// with an error code.
+#define HIP_CHECK(condition)                                                                \
+    {                                                                                       \
+        const hipError_t error = condition;                                                 \
+        if(error != hipSuccess)                                                             \
+        {                                                                                   \
+            std::cerr << "An error encountered: \"" << hipGetErrorString(error) << "\" at " \
+                      << __FILE__ << ':' << __LINE__ << std::endl;                          \
+            std::exit(error_exit_code);                                                     \
+        }                                                                                   \
+    }
+
+#endif
